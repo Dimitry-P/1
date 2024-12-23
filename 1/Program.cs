dd1e0a1696751a53f741aa51a1d1699929ed4544
Dimitry-P <dimitry.piatnitsky@yahoo.com>
2024-12-23 16:36:19 +0300

    Add project files.

---
+++ 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    //•	Задача 1: Напишите программу, которая запрашивает
+    //у пользователя его имя и возраст, а затем выводит
+    //приветственное сообщение с его именем и возрастом через 5 лет.
+    internal class Program
+    {
+        public static string age;
+        public static string name;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сообщите, пожалуйста, ваше имя и возраст:");
+            Console.Write("имя:");
+            name = Console.ReadLine();
+            Console.Write("возраст:");
+            age = Console.ReadLine();
+            int number = int.Parse(age);
+            number += 5;
+            Console.WriteLine("Приветствую, " + name + ", ваш возраст через пять лет: " + number);
+
+            Console.ReadKey();
+        }
+    }
+}
